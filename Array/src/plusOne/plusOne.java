b0e13c2cad0e31ab27ec64be648e516b86d0c269
shanguangff <909278799@qq.com>
2019-12-06 00:15:59 +0800

    11

--- Array/src/plusOne/plusOne.java
+++ Array/src/plusOne/plusOne.java
@@ -12,9 +12,16 @@ public class plusOne {
 
             //数字从末尾开始加一
             digits[i]++;
+            //10以下的数进行对10取余还是本身，为0代表有进位
             digits[i] = digits[i]%10;
+            //如果不为0那说明没有进位，不循环向上一位进行加一
+            if(digits[i]!=0){
+                return digits;
+            }
 
         }
+        digits = new int [digits.length+1];
+        digits[0] = 1;
         return digits;
     }
 }
--- Array/src/plusOne/test.java
+++ Array/src/plusOne/test.java
@@ -1,5 +1,7 @@
 package plusOne;
 
+import java.util.ArrayList;
+
 /**
  * @author cc_ff
  * @creat 2019-12-2019/12/5-23:33
@@ -8,10 +10,11 @@ public class test {
     public static void main(String[] args) {
 
         plusOne plusOne = new plusOne();
-        int [] digits = {9};
+        int [] digits = {9,9,9,9,9,9};
         int[] ints = plusOne.plusOne(digits);
         for (int i = 0; i < ints.length; i++) {
-            System.out.println(digits[i]);
+            System.out.println(ints[i]);
         }
+
     }
 }
